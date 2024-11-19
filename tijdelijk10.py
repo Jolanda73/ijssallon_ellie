9fbadd70ca1bfdb1879b432e2fb0642bc18d7681
Jolanda van Hoorn <j.vanhoorn@bkr.nl>
2024-11-19 14:41:34 +0100

    functies toegevoegd

--- tijdelijk10.py
+++ tijdelijk10.py
@@ -1,7 +1,9 @@
-prijzen = {
-"aardbei": 3,
-"vanille": 4,
-"chocolade": 5} 
+def print_aanbieding():
+	prijzen = {
+	"aardbei": 3,
+	"vanille": 4,
+	"chocolade": 5} 
+
 aanbieding = prijzen ["vanille"] * 0.8
 
 reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € { aanbieding:2f}"
@@ -15,15 +17,8 @@ el = reclame_tekst4
 
 reclame_tekst4 = ["VANDAAG", "IN", "DE", "AANBIEDING", ":", "VANILLE-IJS", "1 LITER", "-", "SLECHTS", "€ 3.20"]
 for el in reclame_tekst4:
-	print (el)
-
-reclame_tekst4 = ["VANDAAG", "IN", "DE", "AANBIEDING", ":", "VANILLE-IJS", "1 LITER", "-", "SLECHTS", "€ 3.20"]
-for el in reclame_tekst4: 
-	print(el.lower())
-
-reclame_tekst4 = ["VANDAAG", "IN", "DE", "AANBIEDING", ":", "VANILLE-IJS", "1 LITER", "-", "SLECHTS", "€ 3.20"]
-for el in reclame_tekst4:
 	if len(el) >= 5: 
 		print(el.upper())
 	else:
 		print(el.lower())
+print_aanbieding()
--- tijdelijk5.py
+++ tijdelijk5.py
@@ -7,3 +7,4 @@ aanbieding = prijzen ["vanille"] * 0.8
 reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € { aanbieding:2f}"
 reclame_tekst2 = reclame_tekst [:63]
 print(reclame_tekst2)
+
